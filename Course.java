85af254ebf2296f97be948c28d570aa216ee0839
Izzy Hasson <izzy.hasson.925@my.csun.edu>
2016-11-09 09:45:52 -0800

    updated Course.java slightly

--- Course.java
+++ Course.java
@@ -1,48 +1,24 @@
 import java.util.LinkedHashSet;
-
+/**
+ * Provides a container for information on a course offered at CSUN.
+ */
 class Course {
-    private String subject;    
-    private String number;    
+    private String subject;
+    private String number;
     private Integer courseID;
-    private String couseName; 
+    private String couseName;
     private Boolean hasLab;
     private int units;
-    
+    private int difficulty; /* for the demo difficulty will just be an int */ 
     private ArrayList<String[]> Prerequisites;
-    
-    private Difficulty estimatedDifficulty = new Difficulty(units);
-    private Difficulty actualDifficulty = new Difficulty(units);
 
-    public Course(){;}
-    
-    public Boolean preReqCheck(ArrayList<String[]>){;}
-    public ArrayList<String[]> givePreReqs(){;}
-}
-
-class Difficulty{
-    LinkedHashSet<String, int>  gradeDist;
-/*
-    int letterA;
-    int aMinus;
-    int bPlus;
-    int letterB;
-    int bMinus;
-    int cPlus;
-    int letterC;
-    int cMinus;
-    int dPlus;
-    int letterD;
-    int dMinus;
-    int fPlus;   //does this even exist?
-    int letterF;
-*/  
-    int units;
-
-    public Difficulty(){
-        gradeDist = new LinkedHashSet<String,int>();
+    public Course(){
+        ;
     }
-    public addGrades(){;}
-    public average(){
-        float gradeVal = 4.0;
-    } 
 
+    public Boolean preReqCheck(ArrayList<String[]>){
+        ;
+    }
+    public ArrayList<String[]> givePreReqs(){
+        ;
+    }
