f0797d7482136e4215ae6fd5126d65daf56b81cc
Michaelh818 <michaelharootoonyan@gmail.com>
2016-11-12 01:05:47 -0800

    Add files via upload

---
+++ SectionUnitTest.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class SectionUnitTest {
+	
+	
+
+	public static void main(String[] args) {
+		
+		String[] notess = {"Wait list is not available for this corequisite course"};
+		ArrayList<String[]> notes = new ArrayList();
+		notes.add(notess);
+		
+		
+		
+		
+		/*  TESTING OF THE CONSTRUCTOR*/
+		Section firstClass = new Section(19378,"COMP 256","LEC","GRD","startdate","enddate","TR","2:00pm","3:15pm",
+				"JD3520",notes,"Lorentz Richard J.",null);
+		
+		
+		
+		Section secondClass = new Section();
+		
+		/*	TESTING OF THE SETTER METHODS*/
+		secondClass.setSectionID(19379);
+		secondClass.setSectionName("COMP 256");
+		secondClass.setComponent("LAB");
+		secondClass.setGradingOption("GRD");
+		secondClass.setStartDate("startdate");
+		secondClass.setEndDate("enddate");
+		secondClass.setDaysOfWeek("TR");
+		secondClass.setStartTime("3:30");
+		secondClass.setEndTime("4:45");
+		secondClass.setLocation("JD3520");
+		secondClass.setNotes(notes);
+		secondClass.setInstructor("Lorentz Richard J.");
+		
+		
+		
+		SectionLinkedList mikes_class_schedule = new SectionLinkedList();
+		
+		mikes_class_schedule.addSection(firstClass);
+		mikes_class_schedule.addSection(secondClass);
+		
+		
+		mikes_class_schedule.printSections();
+
+		
+
+	}
+
+}
