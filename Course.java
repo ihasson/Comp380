3a9c9dd94df4007a0b2f9cdc53cb3599458dffdd
ihasson <ihasson@users.noreply.github.com>
2016-11-18 14:51:22 -0800

    Some comments for clarification added.
    Also a fix for the "String[]" problem.

--- Course.java
+++ Course.java
@@ -3,22 +3,22 @@ import java.util.LinkedHashSet;
  * Provides a container for information on a course offered at CSUN.
  */
 class Course {
-    private String subject;
-    private String number;
-    private Integer courseID;
-    private String couseName;
-    private Boolean hasLab;
-    private int units;
-    private int difficulty; /* for the demo difficulty will just be an int */ 
-    private ArrayList<String[]> Prerequisites;
+    private String subject; //don't need
+    private String number; //don't need
+    private Integer courseID; //don't need
+    private String couseName; //NEED 
+    private Boolean hasLab; //don't need
+    private int units; //need
+    private int difficulty; /* for the demo difficulty will just be an int */ //need
+    private ArrayList<String> Prerequisites; //Need
 
     public Course(){
         ;
     }
 
-    public Boolean preReqCheck(ArrayList<String[]>){
+    public Boolean preReqCheck(ArrayList<String>){
         ;
     }
-    public ArrayList<String[]> givePreReqs(){
+    public ArrayList<String> givePreReqs(){
         ;
     }
