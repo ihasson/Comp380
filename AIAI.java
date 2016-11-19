cf4a477e9b5de39e9e55d20a072d79b1f99950d0
Izzy Hasson <izzy.hasson.925@my.csun.edu>
2016-11-19 12:43:51 -0800

    made some changes to AIAI.java

--- AIAI.java
+++ AIAI.java
@@ -2,14 +2,14 @@
  * Izzy Hasson, other NAMES 
  */
 import java.util.*;
-
-/**
- * Some anonymous helper functions for AIAI.
- */
+import java.util.HashMap;
 /*
  * Global Var Names
  *
  */ 
+/**
+ * Some anonymous helper functions for AIAI.
+ */
 interface Ai {
     public Object funcName(LinkedList<Object> input);
 }
@@ -19,11 +19,14 @@ interface Ai {
  * The actual AI Class. It figures things out and makes decisions.
  */
 class AIAI {
-
-    class CourseOrderPair implements Comparable{
-        String s;
+/*
+ * COP stands for Course Order Pair. It allows one to easily keep track of an 
+ * integer to be temporarilly associated with a Cousrse.
+ */
+    class COP implements Comparable{
+        String s; //maybe change to Course
         int i;
-        public void Pair(String string, int integer){
+        public void COP(String string, int integer){
             s = string;
             i = integer;
         }
@@ -33,60 +36,92 @@ class AIAI {
         public int getInt(){
             return i;
         }
-        public int compareTo(CourseOrderPair p){
+        public void setInt(int a){
+            i=a;
+        }
+        public int compareTo(COP p){
             return this.i - p.getInt();
         }
     }
-    
-    public static void permute(LinkedList lperm, LinkedList lx ) {   
-        if(lx.size() == 0){
-        } else {
-            for(int i = 0; i< lx.size(); i++) {
-                LinkedList newlx = new LinkedList();
-                LinkedList newlperm = new LinkedList();
-                newlperm.addAll(lperm);
-                newlx.addAll(lx);
-                newlperm.add(newlx.remove(i));
-                permute(newlperm, newlx);
-            }
-       // } else {
-       //     //String s = new String();
-       //     for(int i = 0; i< lperm.size(); i++) {
-       //         s = s + lperm.get(i) + "  ";
-       //     }
-       //     out.println(s);
-        }
-    } 
+// I haven't decided whether this will be included  in the final program.    
+//    public static void permute(LinkedList lperm, LinkedList lx ) {   
+//        if(lx.size() == 0){
+//        } else {
+//            for(int i = 0; i< lx.size(); i++) {
+//                LinkedList newlx = new LinkedList();
+//                LinkedList newlperm = new LinkedList();
+//                newlperm.addAll(lperm);
+//                newlx.addAll(lx);
+//                newlperm.add(newlx.remove(i));
+//                permute(newlperm, newlx);
+//            }
+//       // } else {
+//       //     //String s = new String();
+//       //     for(int i = 0; i< lperm.size(); i++) {
+//       //         s = s + lperm.get(i) + "  ";
+//       //     }
+//       //     out.println(s);
+//        }
+//    } 
+    /**
+     * This determines what courses are needed and puts them into an ordered
+     * list so that 
+     */
+
+    public static Planner generatePlanner(Student student){
+    /* Step 1: find all needed courses */
+    /* Step 2: Topological Sort */
+    /* Step 3: Massage data into output format. */
+    }
+    private static ArrayList<COP> topologicalSort(ArrayList<Courses> list){
+        J
+    }
+    private static void topoSort(){
     
     /**
      * Finds the dependencies of a course using depth first search
      */
     public static ArrayList<Course> findDependencies(Course c) {
-        LinkedHashSet<String, Pair> cList = 
-            new LinkedHashMap<String, Pair>();
-        int depth = 0;
+        LinkedHashMap<String, COP> cList = 
+            new LinkedHashMap<String, COP>();
+        int depth = 0; // turns out keeping track of depth is pointless.
         ArrayList<Course> courseList = new ArrayList<>();
-        ArrayList<Pair> pairList= new ArrayList<>();
+        ArrayList<COP> pairList= new ArrayList<>();
         findDependencies(c, cList, depth);
         pairList = clist.toArray();
         pairList.sort();
-        for(Pair p : pairList){
+        for(COP p : pairList){
             courseList.add(GCT.get(p.getName()));
         }
         return courseList;
     }
+    /*
+     * Also finds dependencies but from an array of courses.
+     * Note that it returns an array of COP because I need the ordering.
+     */
+    public static ArrayList<COP> findDependencies(
+                        ArrayList<Course> courseList) {
+        LinkedHashMap<String,COP> cList = new LinkedHashMap<>();
+        for(Course c: courseList) {
+            findDependencies(c, cList, 0);
+        }
+        return cList;
+    }
+    /*
+     * A helper to the other methods of the same name.
+     */
     private static void findDependencies(Course c, 
-            LinkedHashSet<String,Pair> cList, int depth) {
+            LinkedHashMap<String,COP> cList, int depth) {
         if(cList.containsKey(c.getName())){
             return;
         } else {
             depth++;
-            Pair p = new Pair(c.getName, depth);
+            COP p = new COP(c.getName, depth);
             cList.put(c.getName(), p);
             for(String s : c.getPreRequisites()) { 
                 // if get PreRequisites doesn't return a String array then
-                // need to change gthe above.
-                if(!(cList.containsValue(s))){
+                // need to change above.
+                if(!(cList.containsKey(s))){
                     findDependencies(globalCourseTable.get(s), cList, depth);
                     //need to know the name of the global Course Table
                 }
