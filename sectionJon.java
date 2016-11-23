d753af510b0881b7db3ed3a29f956e48255a573a
JC <j_carrasco@icloud.com>
2016-11-23 15:32:44 -0800

    modified Section.java and made sectionJon.java and modified cs_sections.txt to be CSV

---
+++ sectionJon.java
@@ -0,0 +1,20 @@
+public class Section{
+	
+	private String class;
+	private String className;
+	private String type;
+	private String units;
+	private String section;
+	private String location;
+	private String days;
+	private String startTime;
+	private String endTime;
+	private String teacher;
+
+
+	public Section(){
+
+	}
+
+	
+}
