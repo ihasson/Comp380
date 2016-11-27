608719112c1c8f77a0c86ada676e89e32c958b3a
Izzy Hasson <izzy.hasson.925@my.csun.edu>
2016-11-26 16:49:25 -0800

    cleaned up AIAI.java

--- AIAI.java
+++ AIAI.java
@@ -1,22 +1,20 @@
+/* 
+ * Note to Prof. Lingard,
+ * grade topologicalSort();
+ */
 /**
  * Class: AIAI
- *
+ * 
  * Author: Izzy Hasson
  */
-import java.util.*;
-import java.util.HashMap;
-/**
- * Some anonymous helper functions for AIAI.
- */
-interface Ai {
-    public Object funcName(ArrayList<Object> input);
-}
 
+import java.util.*;
 
 /**
  * The actual AI Class. It figures things out and makes decisions.
  */
 public class AIAI {
+
 /*
  * COP stands for Course Order Pair. It allows one to easily keep track of an 
  * integer to be temporarilly associated with a Cousrse.
@@ -45,6 +43,11 @@ public class AIAI {
             return this.i - val;
         }
     }
+
+/*
+ * A simple inner class to make things easier and fake having
+ * actual global variables.
+ */
     class CourseTable {
         private HashMap<String, Course>  gCT = new HashMap<>();
         CourseTable(ArrayList<Course> list){
@@ -56,12 +59,14 @@ public class AIAI {
             return gCT.get(s);
         }
     }
-    /**
-     * A constructor that does nothing so that you can call methods.
-     */
+
+/**
+ * A constructor that does nothing so that you can call methods.
+ */
     public AIAI(){
         ;
     }
+//This is still a work in progress.
     /**
      * This determines what courses are needed and puts them into an ordered
      * list so that 
@@ -83,8 +88,8 @@ public class AIAI {
     /* Step : Massage data into output format. */
         //return plan;
     }
-    /* need to ask Cina to add method to check if course has been taken */
-    /* need to test this since it probably won't work as is. */
+    // need to make sure Sina tested his check course taken method properly
+    // need to test this since it probably won't work as is. 
     private ArrayList<Course> removeCoursesTaken(ArrayList<Course> reqs,
                     Student stu) {   
         for(Course c : reqs){
@@ -94,10 +99,29 @@ public class AIAI {
         }
         return reqs;
     }
-    /* note that elements of prerequisite list point backwards */
+
+/* 
+ * Note to Prof. Lingard,
+ * this is the method to grade.
+ */
+/*
+ * topologicalSort() sorts the courses of the course as follows:
+ * Let the list of Courses passed to this method be a directed 
+ * acyclic graph with each Course object in that list being a node.
+ * Let the prerequisites of each course represesnt edges pointing from
+ * the prerequisite to the node with the prerequisite but only if
+ * said prerequisite is in the graph.
+ * The following algorithm enumerates the number of edges pointing to 
+ * each node. Then it removes all nodes with no antecedents from the 
+ * graph.
+ * The algorithm repeats until there are no more nodes in the graph.
+ */
+/* note that elements of prerequisite list point backwards */
     private  ArrayList<COP> topologicalSort(ArrayList<Course> list,
                     CourseTable gCT){
-    /* Massage data into needed format. */
+    /* 
+     * Massage data into needed format. 
+     */
         LinkedHashMap<String,COP> graph = new LinkedHashMap<>();
         ArrayList<COP> solution = new ArrayList<>();       
         for(Course c : list){
@@ -141,27 +165,28 @@ public class AIAI {
     /**
      * Finds the dependencies of a course using depth first search
      */
-    public  ArrayList<Course> findDependencies(Course c, 
-            CourseTable gCT) {
+    public ArrayList<Course> findDependencies(Course c, CourseTable gCT) {
         LinkedHashMap<String, COP> cList = 
             new LinkedHashMap<String, COP>();
         int depth = 0; // turns out keeping track of depth is pointless.
         ArrayList<Course> courseList = new ArrayList<>();
         ArrayList<COP> pairList= new ArrayList<>();
         findDependencies(c, cList, depth, gCT);
-        //pairList = clist.toArray();
-        //pairList.sort();
+        //pairList = clist.toArray(); //Can't remember what this line was for.
+        //pairList.sort();            //Nor this.
         for(COP p : pairList){
-            courseList.add(gCT.get(p.getString())); //that damned coursetable
+            courseList.add(gCT.get(p.getString()));
         }
         return courseList;
     }
+
     /*
+     * Probably will deprecate.
      * Also finds dependencies but from an array of courses.
      * Note that it returns an array of COP because I need the ordering.
      */
-    public  ArrayList<Course> findDependencies(
-                        ArrayList<Course> courseList, CourseTable gCT) {
+    private  ArrayList<Course> findDependencies(ArrayList<Course> courseList,
+                    CourseTable gCT) {
         ArrayList<Course> solution = new ArrayList<>();
         LinkedHashMap<String,COP> cList = new LinkedHashMap<>();
         for(Course c: courseList) {
@@ -172,11 +197,12 @@ public class AIAI {
         });
         return solution;
     }
+    
     /*
      * A helper to the other methods of the same name.
      */
-    private  void findDependencies(Course c, 
-            LinkedHashMap<String,COP> cList, int depth, CourseTable gCT) {
+    private void findDependencies(Course c, LinkedHashMap<String,COP> cList, 
+                    int depth, CourseTable gCT) {
         if(cList.containsKey(c.getName())){
             return;
         } else {
@@ -193,7 +219,12 @@ public class AIAI {
             }
         }
     }
-    public  void topSortTest(ArrayList<Course> list){
+    
+    /**
+     * This method exists for the sole purpose of testing topologicalSort().
+     * It runs the method and prints the out put.
+     */
+    public void topSortTest(ArrayList<Course> list){
         CourseTable  gCT = new CourseTable(list);
         ArrayList<COP> solution = topologicalSort(list, gCT);
         System.out.println("");
